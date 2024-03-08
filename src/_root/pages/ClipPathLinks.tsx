f88638ae8b05bd2d8b9aabc5db1294ada4e97e5f
Dmitriy Prokhorenko <dima.study.web@gmail.com>
2024-03-08 23:22:13 +0300

    FINISH clip path links

--- src/_root/pages/ClipPathLinks.tsx
+++ src/_root/pages/ClipPathLinks.tsx
@@ -17,11 +17,35 @@ type LinkBoxProps = {
 	href: string;
 };
 
-const NO_CLIP = 'polygon(0 0, 100% 0, 100% 100%, 0 100%)';
-const BOTTOM_RIGHT_CLIP = 'polygon(100% 100%, 100% 0, 100% 100%, 0% 100%)';
-const BOTTOM_LEFT_CLIP = 'polygon(100% 100%, 0 100%, 0 0, 0% 100%)';
-const TOP_LEFT_CLIP = 'polygon(0 0, 0 100%, 0 0, 100% 0)';
-const TOP_RIGHT_CLIP = 'polygon(0 0, 100% 0, 100% 100%, 100% 0)';
+const NO_CLIP = 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)';
+
+const BOTTOM_RIGHT_CLIP = 'polygon(0 0, 100% 0, 0 0, 0% 100%)';
+
+const TOP_RIGHT_CLIP = 'polygon(0 0, 0 100%, 100% 100%, 0% 100%)';
+
+const BOTTOM_LEFT_CLIP = 'polygon(100% 100%, 100% 0, 100% 100%, 0 100%)';
+
+const TOP_LEFT_CLIP = 'polygon(0 0, 100% 0, 100% 100%, 100% 0)';
+
+const ENTRANCE_KEYFRAMES = {
+	left: [BOTTOM_RIGHT_CLIP, NO_CLIP],
+
+	bottom: [BOTTOM_RIGHT_CLIP, NO_CLIP],
+
+	top: [BOTTOM_RIGHT_CLIP, NO_CLIP],
+
+	right: [TOP_LEFT_CLIP, NO_CLIP],
+};
+
+const EXIT_KEYFRAMES = {
+	left: [NO_CLIP, TOP_RIGHT_CLIP],
+
+	bottom: [NO_CLIP, TOP_RIGHT_CLIP],
+
+	top: [NO_CLIP, TOP_RIGHT_CLIP],
+
+	right: [NO_CLIP, BOTTOM_LEFT_CLIP],
+};
 
 const ClipPathLinks = () => {
 	return (
@@ -50,15 +74,59 @@ const ClipPathLinks = () => {
 const LinkBox = ({ Icon, href }: LinkBoxProps) => {
 	const [scope, animate] = useAnimate();
 
+	const getNearestSide = (e) => {
+		const box = e.target.getBoundingClientRect();
+
+		const proximityToLeft = {
+			proximity: Math.abs(box.left - e.clientX),
+
+			side: 'left',
+		};
+
+		const proximityToRight = {
+			proximity: Math.abs(box.right - e.clientX),
+
+			side: 'right',
+		};
+
+		const proximityToTop = {
+			proximity: Math.abs(box.top - e.clientY),
+
+			side: 'top',
+		};
+
+		const proximityToBottom = {
+			proximity: Math.abs(box.bottom - e.clientY),
+
+			side: 'bottom',
+		};
+
+		const sortedProximity = [
+			proximityToLeft,
+
+			proximityToRight,
+
+			proximityToTop,
+
+			proximityToBottom,
+		].sort((a, b) => a.proximity - b.proximity);
+
+		return sortedProximity[0].side;
+	};
+
 	const handleMouseEnter = (e) => {
+		const side = getNearestSide(e);
+
 		animate(scope.current, {
-			clipPath: [BOTTOM_RIGHT_CLIP, NO_CLIP],
+			clipPath: ENTRANCE_KEYFRAMES[side],
 		});
 	};
 
 	const handleMouseLeave = (e) => {
+		const side = getNearestSide(e);
+
 		animate(scope.current, {
-			clipPath: [NO_CLIP, BOTTOM_RIGHT_CLIP],
+			clipPath: EXIT_KEYFRAMES[side],
 		});
 	};
 
